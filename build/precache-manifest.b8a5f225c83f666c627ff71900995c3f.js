e6ea0bc7b729c8e53c9589b9e02caf5da6413cf5
SamirRodrigues <emaildesamir@gmail.com>
2021-12-26 15:54:19 -0300

    deploy

---
+++ build/precache-manifest.b8a5f225c83f666c627ff71900995c3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9223a0a10203eb5bc95eb2a48c7ef51c",
+    "url": "/Sidebar-Modal/index.html"
+  },
+  {
+    "revision": "3bdee9c0d4b8bd6b6e47",
+    "url": "/Sidebar-Modal/static/css/main.e0f79945.chunk.css"
+  },
+  {
+    "revision": "ba2396a95b87985a25bd",
+    "url": "/Sidebar-Modal/static/js/2.336c92e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sidebar-Modal/static/js/2.336c92e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bdee9c0d4b8bd6b6e47",
+    "url": "/Sidebar-Modal/static/js/main.9f7a6ed6.chunk.js"
+  },
+  {
+    "revision": "c0f70a1d470ad68f112d",
+    "url": "/Sidebar-Modal/static/js/runtime-main.f4d072fd.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/Sidebar-Modal/static/media/logo.2bb7da65.svg"
+  }
+]);
